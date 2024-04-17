ab127b14fef5716ac1f1a65beb0d8b9634f8d6bf
itismesujit <sjsharma.pro@gmail.com>
2024-04-17 22:22:30 +0530

    release 1.0.0

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const
   app = require('express')(),
-  PORT = process.env.PORT || 3000;
+  PORT = process.env.PORT || 3000,
+  svgCaptcha = require('svg-captcha');
 
-app.get('/', (req, res) => res.end('success'));
-app.get('/captcha', (req, res) => {
+app
+  .get('/', (req, res) => res.end('success'))
+  .get('/captcha', (req, res) => {
 
-  res.end('End point is working, Go further..')
-})
+    const
+      options = { size: 5, ignoreChars: '0o1i', noise: 1, color: true, },
+      { data, text } = svgCaptcha.create(options);
 
+    res
+      .setHeader('x-text', text)
+      .type('svg')
+      .status(200).send(data);
+  });
 
 app.listen(PORT, console.info(`Server is running on port ${PORT}`));
 
